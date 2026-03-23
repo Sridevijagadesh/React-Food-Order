061b3fc021fe9b050db0405f1888ea15367ea744
sridevijagadesh <sridevi.j2719@gmail.com>
2026-03-23 20:51:31 +0530

    created Lazy statement

---
+++ src/component/Grocery.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Grocery = () => {
+  return (
+    <div>
+      <h1>
+        "i am grocery element i am going to run in different buddler using LAZY
+        function"
+      </h1>
+    </div>
+  );
+};
+
+export default Grocery;
